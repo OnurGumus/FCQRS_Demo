547a17dbe8479f27e47559e0e27c6f48c697849d
Onur Gumus <onur@outlook.com.tr>
2025-03-08 15:13:18 +0100

    bootstrap

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -3,31 +3,7 @@ open Microsoft.Extensions.Logging
 open Hocon.Extensions.Configuration
 open System.IO
 open FCQRS.Model.Data
-
-let configBuilder =
-    ConfigurationBuilder()
-        .AddHoconFile(Path.Combine(__SOURCE_DIRECTORY__, "config.hocon"))
-
-let config = configBuilder.Build()
-
-let loggerF =
-    LoggerFactory.Create(fun builder -> builder.AddConsole() |> ignore) 
-
-let env = new Environments.AppEnv(config,loggerF)
-
-let loggerFactory = env :> ILoggerFactory
-
-let actorApi = FCQRS.Actor.api config loggerFactory
-
-let sagaCheck  _ = []
-
-actorApi.InitializeSagaStarter sagaCheck
-
-let userShard = User.Actor.factory env actorApi
-
-User.Actor.init env actorApi |> ignore
-
-let userSubs =  actorApi.CreateCommandSubscription userShard
+open BootStrap
 
 let register  cid userName =
         let actorId: ActorId = userName |> ValueLens.CreateAsResult |> Result.value
