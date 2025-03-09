5387935c5ce216309802d8e933f392c1a6896ffb
Onur Gumus <onur@outlook.com.tr>
2025-03-09 06:35:57 +0100

    query side

--- src/App/Bootstrap.fs
+++ src/App/Bootstrap.fs
@@ -29,3 +29,20 @@ let userShard = User.Actor.factory env actorApi
 User.Actor.init env actorApi |> ignore
 
 let userSubs cid =  actorApi.CreateCommandSubscription userShard cid
+
+let handleEventWrapper env (offsetValue: int64) (event:obj)=
+    let log = (env :> ILoggerFactory).CreateLogger("Event")
+    log.LogInformation("Event: {0}", event.ToString())
+
+    let dataEvent =
+        match event with
+        | :? Event<User.Event> as  event ->
+            printfn "Event: %A" event
+            []
+        | _ -> []
+
+    dataEvent
+
+let offsetCount =  0
+open FCQRS.Query
+let sub:ISubscribe<obj>  = FCQRS.Query.init actorApi offsetCount (handleEventWrapper env)
